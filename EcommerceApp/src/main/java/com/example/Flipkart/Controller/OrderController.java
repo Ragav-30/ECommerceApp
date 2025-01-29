6e205143c5a8d482011c5b884fbec1c85399fac3
ragav <ragavan3062001@gmail.com>
2025-01-29 22:01:41 +0530

    request mapped for order

--- EcommerceApp/src/main/java/com/example/Flipkart/Controller/OrderController.java
+++ EcommerceApp/src/main/java/com/example/Flipkart/Controller/OrderController.java
@@ -3,12 +3,14 @@ package com.example.Flipkart.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Flipkart.Model.Order;
 import com.example.Flipkart.Service.OrderService;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
 	@Autowired
 	private OrderService orderService;
